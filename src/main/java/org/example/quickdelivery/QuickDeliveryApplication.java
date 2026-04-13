96d592b76b3d0cb758999c631389162219f90dde
YoungJae <apricity1118@Gmail.com>
2026-04-14 00:43:46 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/org/example/quickdelivery/QuickDeliveryApplication.java
@@ -0,0 +1,13 @@
+package org.example.quickdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickDeliveryApplication.class, args);
+    }
+
+}
